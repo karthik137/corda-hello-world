7b510361924590dfc416032ed1ba47f21809a611
Kirito <raokarthik83@gmail.com>
2020-11-19 18:49:12 +0530

    Added annotation

--- contracts/src/main/java/com/template/states/IOUState.java
+++ contracts/src/main/java/com/template/states/IOUState.java
@@ -1,5 +1,7 @@
 package com.template.states;
 
+import com.template.contracts.TemplateContract;
+import net.corda.core.contracts.BelongsToContract;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
 import net.corda.core.identity.Party;
@@ -8,6 +10,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Arrays;
 import java.util.List;
 
+@BelongsToContract(TemplateContract.class)
 public class IOUState implements ContractState {
 
     private final int value;
